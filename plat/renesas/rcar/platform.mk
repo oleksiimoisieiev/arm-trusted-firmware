dfdfcb526dfde476963471e4bc81be580ae22d1d
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2021-09-09 09:57:16 +0000

    rcar: add SCMI reset protocol implementation
    Will be used in ATF SCP implementation.
    
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>

--- plat/renesas/rcar/platform.mk
+++ plat/renesas/rcar/platform.mk
@@ -439,6 +439,7 @@ BL31_SOURCES	+=	${RCAR_GIC_SOURCES}				\
 			drivers/renesas/rcar/scif/scif.S        \
 			drivers/renesas/rcar/console/rcar_printf.c	\
 			drivers/renesas/rcar/delay/micro_delay.c	\
+			plat/renesas/rcar/rcar_scmi_reset.c		\
 			drivers/renesas/rcar/pwrc/call_sram.S		\
 			drivers/renesas/rcar/pwrc/pwrc.c		\
 			drivers/renesas/rcar/common.c			\
