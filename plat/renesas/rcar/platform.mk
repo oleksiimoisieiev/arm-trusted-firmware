7b6670d4bb47e666b4ebb4c9cb9f415c8bc22904
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2021-09-09 10:06:49 +0000

    rcar: add SCMI clock protocol implementation
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>

--- plat/renesas/rcar/platform.mk
+++ plat/renesas/rcar/platform.mk
@@ -440,6 +440,7 @@ BL31_SOURCES	+=	${RCAR_GIC_SOURCES}				\
 			drivers/renesas/rcar/console/rcar_printf.c	\
 			drivers/renesas/rcar/delay/micro_delay.c	\
 			plat/renesas/rcar/rcar_scmi_reset.c		\
+			plat/renesas/rcar/rcar_scmi_clocks.c		\
 			drivers/renesas/rcar/pwrc/call_sram.S		\
 			drivers/renesas/rcar/pwrc/pwrc.c		\
 			drivers/renesas/rcar/common.c			\
