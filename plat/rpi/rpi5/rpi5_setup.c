63c49cda6de8d0242ae5df7d0e73e83182c41ccd
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-07-16 18:19:59 +0300

    plat/rpi/rpi5/rpi5_setup.c
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- plat/rpi/rpi5/rpi5_setup.c
+++ plat/rpi/rpi5/rpi5_setup.c
@@ -5,10 +5,12 @@
  */
 
 #include <rpi_shared.h>
+#include <common/debug.h>
 
 extern void rpi5_init_scmi_server(void);
 
 void plat_rpi_bl31_custom_setup(void)
 {
+	INFO("!!! %s \n", __func__);
 	rpi5_init_scmi_server();
 }
