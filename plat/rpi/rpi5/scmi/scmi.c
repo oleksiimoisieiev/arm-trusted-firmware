6eb1f38ab3128d2eb466524fd4e9ac100fee6b15
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-07-17 19:15:16 +0300

    plat/rpi/rpi5/scmi/scmi.c
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- plat/rpi/rpi5/scmi/scmi.c
+++ plat/rpi/rpi5/scmi/scmi.c
@@ -11,7 +11,7 @@
 
 #include <platform_def.h>
 
-#define RPI_SHM_BASE		0x1ffc00000
+#define RPI_SHM_BASE		0x2e400000
 #define RPI_SHM0_BASE	RPI_SHM_BASE
 
 static struct scmi_msg_channel scmi_channel[] = {
