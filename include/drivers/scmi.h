3e21096b7d42f4843eed1b87048ff2c3388bb5ee
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-07-31 19:37:11 +0300

    include/drivers/scmi.h
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- include/drivers/scmi.h
+++ include/drivers/scmi.h
@@ -12,6 +12,7 @@
 #define SCMI_PROTOCOL_ID_CLOCK			0x14U
 #define SCMI_PROTOCOL_ID_SENSOR			0x15U
 #define SCMI_PROTOCOL_ID_RESET_DOMAIN		0x16U
+#define SCMI_PROTOCOL_ID_PINCTRL		0x19U
 
 /* SCMI error codes reported to agent through server-to-agent messages */
 #define SCMI_SUCCESS			0
